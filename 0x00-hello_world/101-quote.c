d424140e3881bc41abfa9a383fc0cb16d3db71b9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-13 02:38:07 +0000

    Fixed Task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out the last part of a quote. 
+ * main - Prints out the last part of a quote to the standard error.
  * Return: 1 if success.
  */
 int main(void)
 {
-	char quote[]="and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-	write(2,quote,sizeof(quote));
+	char quote[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+
+	write(2, quote, sizeof(quote));
 	return (1);
 }
