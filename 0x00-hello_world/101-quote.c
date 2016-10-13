d69b6c186379e7e5d98807fffd6de6a04a0a25e1
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-13 02:42:41 +0000

    Fixed 19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out the last part of a quote to the standard error.
+ * main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
 int main(void)
