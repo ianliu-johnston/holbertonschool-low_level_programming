c9343bcb4806d4be9c8289063e814b5d6095fd53
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-14 07:48:06 +0000

    fixed 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,17 +14,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("%s %d is %d and is greater than 5\n", last, n, n % 10);
+		printf("greater than 5\n");
 	}
-	else if (n == 0)
+	else if (n % 10 == 0)
 	{
-		printf("%s %d is %d and is 0\n", last, n, n % 10);
+		printf("0\n");
 	}
 	else
 	{
-		printf("%s %d is %d and is less than 6 and not 0\n", last, n, n % 10);
+		printf("less than 6 and not 0\n");
 	}
 	return (0);
 
