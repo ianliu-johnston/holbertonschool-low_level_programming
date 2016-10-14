2f0427aba2ecc0d43f900e04138868fe21f940b9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-14 21:32:14 +0000

    Fixed 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,8 +21,11 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
-				putchar(',');
-				putchar(' ');
+				if(i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
