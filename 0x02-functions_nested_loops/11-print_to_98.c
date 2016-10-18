377770bc36dc594e5b54b8e3df2e8d14c343d00a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-18 17:17:02 +0000

    Finished tasks 11 and 7.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 /**
- * main - 
+ * print_to_98 - print all natural numbers from n to 98.
  *
- * Return:
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for(n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for(n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	putchar('\n');
+}
+
+/**
+ * main - test functions for print_to_98
+ *
+ * Return: 0 on success.
  */
 
 int main(void)
 {
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
 	return (0);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,14 @@
-#include <stdio.h>
+int _putchar(char c);
+int _abs(int);
+/**
+ * print_last_digit - prints the last digit of a numer.
+ * Return: The last digit.
+ */
+int print_last_digit(int n)
+{
+	_putchar(_abs((n % 10)) + '0');
+	return (_abs(n % 10));
+}
 /**
  * main - 
  *
@@ -7,5 +17,12 @@
 
 int main(void)
 {
+	int r;
+
+	print_last_digit(97);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
 	return (0);
 }
