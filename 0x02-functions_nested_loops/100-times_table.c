22c5831f2eaa1bd8de1b94682b8c2d8636de69cc
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-18 22:49:56 +0000

    Fixed 100.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_times_table - prints the times table from 0 - 9.
+ * print_times_table - prints the times table for n.
  * @n: The multiplication table requested.
  * Return: Nothing.
  */
@@ -8,9 +8,7 @@ void print_times_table(int n)
 {
 	int i, j, res;
 
-	if (n > 15 || n < 0)
-		;
-	else
+	if (!(n > 15 || n < 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
