8b94d1280212267ff099510a2a608d3b63dda302
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-18 08:09:44 +0000

    Formatted desired output for Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,15 +13,20 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			res = (i * j);
-
-			_putchar((res / 10) + '0');
-			_putchar((res % 10) + '0');
-
-			if (j < 9)
+			if(j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+			}
+			if(res <= 10)
+			{
 				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
 			}
 		}
 		_putchar('\n');
