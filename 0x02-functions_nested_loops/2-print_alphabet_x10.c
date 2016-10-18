db584253de4a63fbb32af15e50cb64bdd5616368
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-18 20:34:25 +0000

    Fixed task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,5 @@
 #include "holberton.h"
 /**
- * print_alphabet - prints the English alphabet, from a-z
- * Return: Nothing.
- */
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
-}
-
-/**
   * print_abc_10x - repeats the print_alphabet 10 times.
   * Return: Nothing.
   */
