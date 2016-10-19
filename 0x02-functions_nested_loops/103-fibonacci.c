f18b9b752bc603244ccce30869cd8064c28e1c8b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-19 05:25:53 +0000

    Added 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,19 +1,24 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
-  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+  * main - sum even fibonacci numbers under 4 million.
   * Return: Nothing.
   */
-void print_alphabet_x10(void)
+int main(void)
 {
-	int i;
-	char c;
+	unsigned long count, i, j, k, sums;
 
-	for (i = 0; i < 10; ++i)
+	i = sums = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		k = i + j;
+		i = j;
+		j = k;
+		if (k % 2 == 0 && k < 4000000)
 		{
-			_putchar(c);
+			sums += k;
 		}
-		_putchar('\n');
 	}
+	printf("%lu\n", sums);
+	return (0);
 }
