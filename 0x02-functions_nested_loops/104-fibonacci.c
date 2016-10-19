aac999064775b99d3bc276f27343332f159ef12d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-19 22:46:56 +0000

    Finished 104!

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,19 +1,45 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
-  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+  * main - print the first 98 fibonacci numbers.
   * Return: Nothing.
   */
-void print_alphabet_x10(void)
+int main(void)
 {
-	int i;
-	char c;
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
 
-	for (i = 0; i < 10; ++i)
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
 	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
+	return (0);
 }
