44419257dc8f62eeb47fcf8e6fa46978e6bfdf14
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-19 01:10:43 +0000

    Fixed 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 	
 	i = 0;
 	j = 1;
-	for (count = 0; count <= 50; count++)	
+	for (count = 0; count < 50; count++)	
 	{
 		k = i + j;
 		i = j;
 		j = k;
 		printf("%lu", k);
-		if (count >= 50)
+		if (count == 49 )
 			putchar('\n');
 		else
 		{
