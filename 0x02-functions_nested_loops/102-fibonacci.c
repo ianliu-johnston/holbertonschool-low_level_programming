c1017cf79432820d9af9cf2c03262e24432bf113
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-19 01:12:55 +0000

    Fixed 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
   * main - print the first 50 fibonacci numbers.
   * Return: Nothing.
@@ -6,16 +6,16 @@
 int main(void)
 {
 	unsigned long count, i, j, k;
-	
+
 	i = 0;
 	j = 1;
-	for (count = 0; count < 50; count++)	
+	for (count = 0; count < 50; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
 		printf("%lu", k);
-		if (count == 49 )
+		if (count == 49)
 			putchar('\n');
 		else
 		{
