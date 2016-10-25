a117c2eb735539fcd73e8cdcecfcde90acd096c8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-25 14:08:44 -0700

    Finished task 5

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,25 @@
-#import "holberton.h"
 /**
- * main -
- * Return:
+ * rev_string - reverses a string.
+ * @str: input string to reverse.
  */
-int main(void)
+void rev_string(char *s)
 {
-	return (0);
+	int i, j;
+	char r[1000];
+
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		r[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[i] = r[j];
+		i--;
+		j++;
+	}
+	s[j++] = '\0';
 }
