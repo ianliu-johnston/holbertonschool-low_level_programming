82b5cc658ac83783e88d214228a292422b5cddd7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-25 21:15:05 -0700

    Added 100, but did not finish

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,35 @@
-#import "holberton.h"
+#include <stdio.h>
+#include "holberton.h"
 /**
- * main -
- * Return:
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
  */
-int main(void)
-{
-	return (0);
+int _atoi(char *s)
+{	
+	short boolean;
+	int i, j, minus, result, expo;
+	char temp;
+	
+	i = j = minus = result = boolean = 0;
+	expo = 10;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			expo *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= expo;	
+			if(expo == 10)
+				result += (s[i] - '0');
+			else
+				result -= (s[i] - '0');
+			boolean = 1;
+		}
+		i++;
+		if ((s[i] < '0' || s[i] > '9') && boolean == 1)
+			break;
+
+	}
+	return (result);
 }
