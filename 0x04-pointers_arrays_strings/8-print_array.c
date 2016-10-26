6e4f6c9fe9b809b65305dac0ee332eff4edf5e1c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-25 18:27:02 -0700

    Fixed 8

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,10 @@ void print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-		printf("%d", a[i]);
 		if (i != n - 1)
-			printf(", ");
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
 		i++;
 	}
 	putchar('\n');
