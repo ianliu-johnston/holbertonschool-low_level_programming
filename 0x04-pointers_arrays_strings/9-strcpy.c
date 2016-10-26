fc72babfe0cda2fa279dae2c7edffe7b52775fc1
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-25 17:27:44 -0700

    Finished task 9

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,18 @@
-#import "holberton.h"
 /**
- * main -
- * Return:
+ * *_strcpy - copies string pointed to by src to buffer pointed to by dest
+ * @dest: the pointer to a string
+ * @src: the pointer to a buffer
+ * Return: the value of the pointer to dest.
  */
-int main(void)
+char *_strcpy(char *dest, char *src)
 {
-	return (0);
+	int i;
+
+	i = 0;
+	while (src[i] >= '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
 }
