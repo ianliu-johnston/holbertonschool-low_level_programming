5954985db636784caf68f9039ad66a9277b6787b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-25 21:21:21 -0700

    Fixed 100.

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
  * _atoi - converts a string to an integer.
  * @s: the string to convert
+ * Return: the converted integer.
  */
 int _atoi(char *s)
-{	
+{
 	short boolean;
 	int i, j, minus, result, expo;
 	char temp;
-	
+
 	i = j = minus = result = boolean = 0;
 	minus = 1;
 	expo = 10;
@@ -21,8 +21,8 @@ int _atoi(char *s)
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			result *= expo;	
-			if(expo == 10)
+			result *= expo;
+			if (expo == 10)
 				result += (s[i] - '0');
 			else
 				result -= (s[i] - '0');
@@ -31,7 +31,6 @@ int _atoi(char *s)
 		i++;
 		if ((s[i] < '0' || s[i] > '9') && boolean == 1)
 			break;
-
 	}
 	result *= minus;
 	return (result);
