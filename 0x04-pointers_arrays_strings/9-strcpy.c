db9c133f3670d621d8cdfddd5515547302517f79
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-25 17:32:57 -0700

    Fixed 9

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,11 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (src[i] >= '\0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
