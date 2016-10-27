853a06973537df8957fe6c7d0b3bdc8937706ae1
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-27 15:10:34 -0700

    Fixed task 3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,14 @@
 /**
- * _strncpy - copies a string
- * @dest: Destination string
- * @src: source string
- * Return: pointer to copied string.
+ * _strcmp - compares two strings, ending at the first byte that is different.
+ * @s1: string to be compared
+ * @s2: string to compare by
+ * Return: the difference between the first character that are not the same
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, n;
-	
-	for (i = n = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
 			;
 	return (s1[i] - s2[i]);
 }
