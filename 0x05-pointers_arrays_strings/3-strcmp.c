bfa8bea8305d564a4e7f89b236160cb5c51a0f50
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-27 15:13:44 -0700

    Fixed 3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] == s2[i]; i++)
-			;
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+		;
 	return (s1[i] - s2[i]);
 }
