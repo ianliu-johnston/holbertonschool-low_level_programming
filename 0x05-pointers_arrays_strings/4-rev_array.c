4ac28b3442bafcb5d0dadf1a2a48626975ca1c6f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-27 15:52:20 -0700

    Finished 4

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - reverses the content of an array of integers. 
+ * @a: the array to reverse
+ * @n: size of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int b[1024];
+
+	i = 0;
+	n -= 1;
+	while (n >= 0)
+	{
+		b[i] = a[n];
+		n--;
+		i++;
+	}
+	b[i] = '\0';
+	for ( ; i >= 0; i--)
+		a[i] = b[i];
+}
