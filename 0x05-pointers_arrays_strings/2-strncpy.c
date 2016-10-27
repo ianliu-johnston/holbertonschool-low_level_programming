e5c3b4c9e3bb9ecf34f18bc6b595683e8a851fde
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-27 14:43:29 -0700

    Fixed task 2

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -16,6 +16,3 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i++] = '\0';
 	return (dest);
 }
-
-
-/** Good taste is about seeing the big patters and instinctively knowing the right way to do things.*/
