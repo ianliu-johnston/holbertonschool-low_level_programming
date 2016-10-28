51c6606c8976bb56640ffe5323fee30b7b3d54a7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-27 17:27:05 -0700

    Fixed 5

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -6,11 +6,14 @@
  */
 char *string_toupper(char *c)
 {
-	while (*c != '\0')
+	int i;
+
+	i = 0;
+	while (c[i] != '\0')
 	{
-		if (*c >= 'a' && *c <= 'z')
-			*c -= 'a' - 'A';
-		c++;
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] -= 'a' - 'A';
+		i++;
 	}
 	return (c);
 }
