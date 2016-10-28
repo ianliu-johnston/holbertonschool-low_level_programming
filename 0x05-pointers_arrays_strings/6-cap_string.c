db8c6d0e4cb61626f8a4b2b9f2c23a92ba031a4d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-27 19:22:06 -0700

    Finished 6.

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -6,18 +6,16 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	char sep[] = " \t,;.!?\"(){}\n";
+	char sep[] = " \t\n,;.!?\"(){}";
 
 	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
 	while (s[i] != '\0')
 	{
-		j = 0;
-		while (sep[j] != '\0')
-		{
+		for (j = 0; sep[j] != '\0'; j++)
 			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
 				s[i] -= ('a' - 'A');
-			j++;
-		}
 		i++;
 	}
 	return (s);
