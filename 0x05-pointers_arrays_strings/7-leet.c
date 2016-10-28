123e63b53fc4fc092f26a9f9ce36275c7141110d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-27 19:37:38 -0700

    Finished Task 7

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+/**
+ * leet - encodes a string into leetspeek.
+ * @s: pointer to input string.
+ * Return: Returns pointer to leetspeek string.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char subs[] = "aAeEoOtTlL";
+	char le[] = "43071";
+
+	i = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; subs[j] != '\0'; j++)
+			if (s[i] == subs[j])
+				s[i] = le[j / 2];
+	}
+	return (s);
+}
+
+/*
+ * only use one if, no switch statements.
+(*
+ * a, A = 4
+ * e, E = 3
+ * l, L = 1
+ * o, O = 0
+ * t, T = 7
+ */
