a2d49b47d7170a653e92655c0123f6e8d0f2f5da
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-28 09:03:21 -0700

    Finished 8

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -8,13 +8,18 @@ char *rot13(char *s)
 	int i, j;
 	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char boolean;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; alpha[j] != '\0'; j++)
+		boolean = 0;
+		for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
 		{
 			if (s[i] == alpha[j])
+			{
 				s[i] = r[j];
+				boolean = 1;
+			}
 		}
 	}
 	return (s);
