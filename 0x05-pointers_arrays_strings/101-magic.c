9b0801a9254654e6a2fa83014d4efc105f38a94e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-29 12:03:25 -0700

    Update 101-magic.c

--- 0x05-pointers_arrays_strings/101-magic.c
+++ 0x05-pointers_arrays_strings/101-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98; /* --use: *(p + (&a[2] - &n)) = 98 if you need to declare more vars.*/
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   return (0);
 }
