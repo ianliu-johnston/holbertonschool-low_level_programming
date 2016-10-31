f10386d02813607009d936380ef7e392d94bdb93
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-31 13:47:45 -0700

    Fixed 2, maybe.

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -8,9 +8,11 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0' && *s != c)
+	while (*s != '\0')
 	{
-		if(*s == c)
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
 			return (s + 1);
 		s++;
 	}
