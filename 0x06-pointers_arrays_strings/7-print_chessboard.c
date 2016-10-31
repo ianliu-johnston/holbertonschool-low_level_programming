21ff611436ed7e4c14d000adc361f6764984bf5e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-31 10:51:39 -0700

    Filled empty files with c template.

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,13 @@
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to memory area 's'
+ * @b: constant byte to fill
+ * @n: number of bytes to fill
+ * Return: pointer to memory area 's'
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	return (s);
+}
