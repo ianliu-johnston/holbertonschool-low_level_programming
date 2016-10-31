9c2b7d0722acaff507e526ab55341422871edc11
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-31 14:12:14 -0700

    Fixed 4

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,6 @@
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: pointer to string to search through 
+ * @s: pointer to string to search through
  * @accept: array of bytes to search for
  * Return: pointer to the byte in s that matches one of the bytes in accept
  * or return NULL if no bytes are found.
@@ -14,8 +14,8 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (i = 0; accept[i] != '\0'; i++)
 		{
-		       if (*s == accept[i])
-			       return (s);
+			if (*s == accept[i])
+				return (s);
 		}
 		s++;
 	}
