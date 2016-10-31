007ee7f7e7151310dcc0c0de75d8e0ca4838b8d7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-31 14:07:09 -0700

    Finished 4

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,23 @@
 /**
- * _memset - fills memory with a constant byte
- * @s: pointer to memory area 's'
- * @b: constant byte to fill
- * @n: number of bytes to fill
- * Return: pointer to memory area 's'
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer to string to search through 
+ * @accept: array of bytes to search for
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or return NULL if no bytes are found.
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_strpbrk(char *s, char *accept)
 {
+	unsigned int i;
 
-	return (s);
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+		       if (*s == accept[i])
+			       return (s);
+		}
+		s++;
+	}
+	return (0);
 }
