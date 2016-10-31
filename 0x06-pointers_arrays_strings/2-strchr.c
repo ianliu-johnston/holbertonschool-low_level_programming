a1e12ade6402acb3475685dff3b9e98dd2d3cc55
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-31 12:55:23 -0700

    Fixed 2 for bettychecks

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 /**
- * _strchr - locates a character in a string 
+ * _strchr - locates a character in a string
  * @s: pointer to string
  * @c: character to locate
  * Return: pointer to first occurence of character c
  * or NULL if character is not found.
  */
 
-char *_strchr(char *s, char c) 
+char *_strchr(char *s, char c)
 {
 	while (*s != '\0' && *s != c)
 		s++;
