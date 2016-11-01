e1398343fe60d5c8b92ec7167ca3c1797ebf1ba7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-01 12:40:03 -0700

    Fixed 5

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,33 @@
+#include "holberton.h" 
 /**
  * _strstr - locates a substring
  * @haystack: string to look though
  * @needle: substring to find
  * Return: pointer to string with result of search
  */
+int _strncmp (char *s1, char *s2, unsigned int b)
+{
+	unsigned int i;
+
+	for (i = 0; i <= b && s1[i] == s2[i] && *s1 && *s2; i++)
+		;
+	return (i - b);
+}
+
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-
+	int len;
+/*Get length of needle for strncmp*/
+	len = 0;
+	while (needle[len])
+		len++;
+	/*compare substring*/
 	while (*haystack != '\0')
 	{
-		i = 0;
-		if (needle[i] != *haystack)
-			haystack++;
-		else /* needle[i] == *haystack */
-		{
-			i = 0;
-			while (*(needle + i) == *(haystack + i))
-			{
-				if (*(needle + i) == *(haystack + i) && *(needle + i + 1) == '\0')
-					return (needle);
-				i++;
-			}
-			haystack += i;
-		}
+		if (_strncmp(haystack, needle, len) == 0)
+			return (haystack);
+		haystack++;
 	}
-	return (0);
+	return (NULL);
 }
