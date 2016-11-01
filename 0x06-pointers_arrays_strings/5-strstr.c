d738366cc56abc80db1acca0492220c0ce63fb31
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-31 22:57:46 -0700

    Fixed 5.

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  * _strstr - locates a substring
  * @haystack: string to look though
@@ -10,10 +9,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
-	i = 0;
 	while (*haystack != '\0')
 	{
-		printf("hay: %s ==> needle: %s\n", haystack, needle);
+		i = 0;
 		if (needle[i] != *haystack)
 			haystack++;
 		else /* needle[i] == *haystack */
@@ -21,7 +19,6 @@ char *_strstr(char *haystack, char *needle)
 			i = 0;
 			while (*(needle + i) == *(haystack + i))
 			{
-				printf("n: %c --> h: %c\n", *(needle + i), *(haystack + i));
 				if (*(needle + i) == *(haystack + i) && *(needle + i + 1) == '\0')
 					return (needle);
 				i++;
