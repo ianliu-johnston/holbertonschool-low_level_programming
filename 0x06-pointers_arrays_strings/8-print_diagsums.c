fbac8528de0bb0c346b5c20857d3ed69e8c63fa4
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-31 23:01:19 -0700

    Fixed 8

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -2,29 +2,29 @@
 /**
  * print_diagsums - prints the sum of the two diagonals of a square matrix
  * @a: pointer to 2D array.
- * @size: which diagonal to add. 
+ * @size: which diagonal to add.
  */
 
-void print_diagsums(char *a, int size) 
+void print_diagsums(char *a, int size)
 {
 	int i, j, resx, resy;
 
-        i = resx = resy = 0;
+	i = resx = resy = 0;
 	while (i <= size)
 	{
-		while (j <= size )
+		while (j <= size)
 		{
 			if (i == j)
-                            resx += a[i][j];
-			j++;
+				resx += a[i][j];
+		j++;
 		}
-                while (j >= 0)
-                {
-                        if (i == j)
-                            rest += a[i][j];
-                        j--;
-                }
+		while (j >= 0)
+		{
+			if (i == j)
+				resy += a[i][j];
+			j--;
+		}
 		i++;
 	}
-        printf("%d, %d\n", resx, resy);
+	printf("%d, %d\n", resx, resy);
 }
