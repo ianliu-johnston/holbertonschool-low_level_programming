27e6d914fe5fb8f432d1561ab2d8075428202f5f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-01 12:47:44 -0700

    Fixed 5.

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,13 @@
-#include "holberton.h" 
+#include "holberton.h"
 /**
- * _strstr - locates a substring
- * @haystack: string to look though
- * @needle: substring to find
- * Return: pointer to string with result of search
+ * _strncmp - compares two strings within b bytes.
+ * @s1: string to check
+ * @s2: string to check from
+ * @b: number of bytes to check
+ * Return: 0 if strings are the same
+ * any other number if they are different.
  */
-int _strncmp (char *s1, char *s2, unsigned int b)
+int _strncmp(char *s1, char *s2, unsigned int b)
 {
 	unsigned int i;
 
@@ -13,8 +15,12 @@ int _strncmp (char *s1, char *s2, unsigned int b)
 		;
 	return (i - b);
 }
-
-
+/**
+ * _strstr - locates a substring
+ * @haystack: string to look though
+ * @needle: substring to find
+ * Return: pointer to string with result of search
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	int len;
