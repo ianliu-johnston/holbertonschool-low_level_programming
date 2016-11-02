40dc117dd6abbd11c54c056bf8be87bfe9195e1a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-02 16:58:52 -0700

    Finished 1

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,11 +1,13 @@
+#include "holberton.h"
 /*
  * _print_rev_recursion - prints a string in reverse
  * @s: the string to print
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	s++;
-	_print_rev_recursion(s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
