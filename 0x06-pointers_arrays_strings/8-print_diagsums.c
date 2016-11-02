e242b1876d4018527377ce5069c10a973ab182ec
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-01 18:35:37 -0700

    Fixed 8

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -9,6 +9,7 @@ void print_diagsums(int *a, int size)
 {
 	int i, sumx, sumy;
 
+	sumx = sumy = 0;
 	for (i = 0; i < (size * size); i += size + 1)
 		sumx += a[i];
 	for (i = size - 1; i < (size * size - 1); i += size - 1)
