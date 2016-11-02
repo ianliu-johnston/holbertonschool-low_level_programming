6ec853b6e47f38a652281eae5fe6906f3e4b9647
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-02 12:16:56 -0700

    Finished task 3

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -1,10 +1,13 @@
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-int _putchar.c(char c);
+ /**
+ * factorial - returns the factorial of the number
+ * @n: the max factorial
+ * Return: factorial of number n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
+		return (1);
+	return (n * factorial(n-1));
+}
