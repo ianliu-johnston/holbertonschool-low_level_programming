23573a1be454030b51193f1cd3dbc37bf834318f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-03 00:06:20 -0700

    Finished 7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -26,10 +26,7 @@ int checker(char *str, int len, int count)
 	if (count >= len)
 		return (1);
 	if (str[len] == str[count])
-	{
-		printf("in checker: %d, %d\n", len, count);
 		return (checker(str, len - 1, count + 1));
-	}
 	return (0);
 }
 /**
@@ -41,6 +38,5 @@ int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
 	int count = 0;
-printf("len: %d\n", len);
 	return (checker(s, len - 1, count));
 }
