26b5439f1a004b63329f74ef32a35df06ed19bc9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-02 22:26:53 -0700

    Finished 6

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 /**
+  * checker - checks recursively the input from is_prime_number
+  * @n: iterator
+  * @base: base number to check
+  * Return: 1 if n is a prime, else return 0 otherwise.
+  */
+int checker(int n, int base)
+{
+	if (base % n == 0 || base < 2)
+		return (0);
+	else if (n == base - 1)
+		return (1);
+	else if (base > n)
+		return (checker(n + 1, base));
+	return (1);
+}
+/**
   * is_prime_number - checks if the number is a prime number
   * @n: the number to check
   * Return: 1 if n is a prime, else return 0 otherwise.
   */
 int is_prime_number(int n)
 {
-	if (n < 0)
-		return (0);
-	else if (is_prime_number(n - 1) % n == 0)
-		return (1);
-	return (0);
+	return (checker(2, n));
 }
