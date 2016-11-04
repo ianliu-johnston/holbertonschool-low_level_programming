e6a5502dc377f387d103e7d86036222ff1a0c834
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-04 12:32:58 -0700

    Finished 3

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -1,11 +1,24 @@
- * _print_rev_recursion - prints a string in reverse
- * @s: the string to print
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers and prints the result
+ * @argc: argument count must be exactly two numbers
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
  */
-void _print_rev_recursion(char *s)
+int main(int argc, char *argv[])
 {
-	if (*s)
+	int res1, res2;
+	
+	res1 = res2 = 0;
+	if (argc == 3)
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		res1 = atoi(argv[1]);
+		res2 = atoi(argv[2]);
+		printf("%d\n", res1 * res2);
+		return (0);
 	}
+	else
+		printf("Error\n");
+	return (1);
 }
