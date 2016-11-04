2441e5996568cfbf5022adb3b8d1b993264f40f7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-04 12:12:30 -0700

    Finished task 2

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,11 +1,14 @@
- * _print_rev_recursion - prints a string in reverse
- * @s: the string to print
- */
-void _print_rev_recursion(char *s)
+#include <stdio.h>
+/**
+ * main - prints out all of the arguments passed to it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */ int main(int argc, char *argv[])
 {
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
 }
