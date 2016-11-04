4fbea37846b0e3cb5c54d988f1e4a5c3df682a5d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-04 12:08:21 -0700

    Finished Task 1

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -2,12 +2,12 @@
 /**
  * main - prints out all of the arguments passed to it.
  * @argc: argument count
- * @argv: argument vector 
+ * @argv: argument vector
  * Return: 0 on success
  */
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
-
+	argv[0] = 0;
 	return (0);
 }
