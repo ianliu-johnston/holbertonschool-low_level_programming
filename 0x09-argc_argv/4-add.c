25838507ce0c922ce80eb69564cd5aaa3c6152ea
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-04 17:41:02 -0700

    Finished 4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,11 +1,41 @@
- * _print_rev_recursion - prints a string in reverse
- * @s: the string to print
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * checker - checks for valid input
+ * @argc: argument count
+ * @i: counter for argv[]
+ * @j: counter for argv[][]
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
  */
-void _print_rev_recursion(char *s)
+int checker(int argc, int i, unsigned int j, char *argv[])
 {
-	if (*s)
+	for (i = 1; i <= argc; i++)
+		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
+			if (isdigit(argv[i][j]) == 0)
+				return (1);
+	return (0);
+}
+/**
+ * main - adds all arguments together if they are digits.
+ * @argc: argument count only accepts ints separated by spaces.
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int result, i;
+
+	result = 0;
+	if (checker(argc, 1, 0, argv) == 1)
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		printf("Error\n");
+		return (1);
 	}
+	for (i = 1; i < argc; i++)
+		result += atoi(argv[i]);
+	printf("%d\n", result);
+	return (0);
 }
