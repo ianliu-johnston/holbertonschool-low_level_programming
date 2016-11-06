9b371730fc29d644eac8ee41d2b402a391fc5212
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-06 12:51:35 -0800

    Finished 100

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * main - prints out all of the arguments passed to it.
+ * checker - checks for valid input
+ * @argc: argument count
+ * @i: counter for argv[]
+ * @j: counter for argv[][]
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
+ */
+int checker(int argc, int i, unsigned int j, char *argv[])
+{
+	for (i = 1; i <= argc; i++)
+		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
+			if (isdigit(argv[i][j]) == 0)
+				return (1);
+	return (0);
+}
+/**
+ * main - Prints the minimum number of coins
+ * to make change for an amount of cents.
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
-	argv[0] = 0;
+	unsigned int cents;
+	int coins;
+
+	cents = coins = 0;
+	if (argc == 2 && checker(argc, 1, 0, argv) == 0)
+	{
+		cents = atoi(argv[1]);
+		for ( ; cents >= 25; coins++, cents -= 25)
+			;
+		for ( ; cents >= 10; coins++, cents -= 10)
+			;
+		for ( ; cents >= 5; coins++, cents -= 5)
+			;
+		for ( ; cents >= 2; coins++, cents -= 2)
+			;
+		for ( ; cents >= 1; coins++, cents--)
+			;
+		printf("%d\n", coins);
+	}
+	else
+		printf("Error\n");
 	return (0);
 }
