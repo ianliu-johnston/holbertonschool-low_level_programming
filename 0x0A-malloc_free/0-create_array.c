6855523c6cbc874234d37ce60cd465d741525d2a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-08 14:18:35 -0800

    Finished 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
-	p = malloc(size * sizeof(char));
+	p = (char *)malloc(size * sizeof(char));
 	for (i = 0; i < size * sizeof(char); i++)
 		p[i] = c;
 	return (p);
