f17a0d6eb61212d6013ddc8b520db96e5b0873bb
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-08 16:11:17 -0800

    Fixed whitespace for 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,11 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * str_concat - concatenates two strings
- * @str: the string to copy
- * Return: pointer to a new buffer
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to a new buffer containing s1 and s2
  */
-char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2)
 {
+	char *p;
+	int s1count, s2count, sizeBuffer, i;
 
-	return ();	
+	for (s1count = 0; s1[s1count]; s1count++)
+		;
+	for (s2count = 0; s2[s2count]; s2count++)
+		;
+	sizeBuffer = s1count + s2count + 1;
+	p = (char *)malloc(sizeBuffer * sizeof(char));
+	for (i = 0; i < sizeBuffer; i++)
+		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
+	p[sizeBuffer] = '\0';
+	return (p);
 }
