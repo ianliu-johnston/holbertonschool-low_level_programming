57ad6853c2aaa80cc2a418065b5a83130153fe15
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-08 16:59:59 -0800

    Finished 5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include "holberton.h"
 /**
  * argstostr - concatenates all arguments of program.
  * @ac: argument count
@@ -7,6 +9,22 @@
  */
 char *argstostr(int ac, char **av)
 {
-	return (0);
+	int i, j, count, totalCount;
+	char *p;
+
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			count++;
+	}
+	p = (char *)malloc(count * sizeof(char) + ac + 1);
+	for (i = 1; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++, totalCount++)
+			p[totalCount] = av[i][j];
+		p[totalCount] = '\n';
+		totalCount++;
+	}
+	return (p);
 }
 
