061225fb1d23a28780bfa2fa1f4c9dd7b25814b6
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-08 17:54:34 -0800

    Finished 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,12 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * alloc_grid - creates a 2-D integer grid
- * @width: width of grid 
+ * @width: width of grid
  * @height: height of grid
  * Return: pointer to 2-D integer grid
  */
 
 int **alloc_grid(int width, int height)
 {
-	return (0);
+	int i, j;
+	int **p;
+
+	p = (int **)malloc(width * sizeof(p));
+	for (i = 0; i < width; i++)
+	{
+		p[i] = malloc(height * sizeof(int));
+		for (j = 0; j < height; j++)
+			p[i][j] = 0;
+	}
+	return (p);
 }
