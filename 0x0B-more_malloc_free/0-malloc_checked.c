5b0071216e42701cdb97ffa5d0bea80f52fcbaae
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-10 06:32:52 -0800

    Initial commit

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+/**
+  * name - description
+  * @var - variable and what it does
+  * Return: return 0 on success
+  */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x0B-more_malloc_free/README.md
@@ -0,0 +1,13 @@
+# Holberton-School_0x0A-malloc_free
+
+0-malloc_checked.c
+
+1-string_nconcat.c
+
+2-calloc.c
+
+3-array_range.c
+
+100-realloc.c
+
+101-mul.c
