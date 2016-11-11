31e822636f5f7aff40a8cc282e8eaea7552c21d2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-10 23:19:45 -0800

    Task 1: done

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,14 +1,32 @@
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * string_nconcat - description
- * @s1: variable and what it does
- * @s2:
- * @n:
- * Return: return 0 on success
+ * string_nconcat - concatenate two strings up to n bytes.
+ * @s1: source string
+ * @s2: string to truncate up to n bytes
+ * @n: number of bytes to truncate by
+ * Return: pointer to new buffer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
+	unsigned int s1count, s2count, sizeBuffer, i;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (s1count = 0; s1[s1count]; s1count++)
+		;
+	for (s2count = 0; s2[s2count]; s2count++)
+		;
+	s2count > n ? (s2count = n) : (n = s2count);
+	sizeBuffer = s1count + s2count + 1;
+	p = malloc(sizeBuffer * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < sizeBuffer; i++)
+		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
+	p[sizeBuffer] = '\0';
 	return (p);
 }
