b1e3506dad607b59b16123359c2edfea70ab2781
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-10 23:34:26 -0800

    Finished task 2

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -1,10 +1,21 @@
 #include <stdlib.h>
 /**
-  * name - description
-  * @var - variable and what it does
-  * Return: return 0 on success
+  * _calloc - allocates memory for an initialized buffer
+  * @nmemb - number of memory bytes
+  * @size - number of data type bits
+  * Return: return pointer to initialized buffer
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	return (0);
+	int *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return(NULL);
+	for (i = 0; i < nmemb ; i++)
+		p[nmemb] = 0;
+	return (p);
 }
