dcc8e39eb4567f2b857a5f4cbf71a833e00a47c7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-11 09:11:06 -0800

    Task 0: used exit for error codes

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -8,13 +8,8 @@ void *malloc_checked(unsigned int b)
 {
 	void *p;
 
-	if (b > 1)
-	{
-		p = malloc(b);
-		if (p == NULL)
-			return (NULL);
-	}
-	else
-		return (NULL);
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
 	return (p);
 }
