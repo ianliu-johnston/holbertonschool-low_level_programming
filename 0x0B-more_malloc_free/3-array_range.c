754850f7e33bc9893f462e8062f4a1c133355b3e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-11 00:07:26 -0800

    Task 3: accounted for negatives

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -1,20 +1,32 @@
 #include <stdlib.h>
+#include <stdio.h>
 /**
   * array_range - creates an array of integers
   * @min: minimum value
   * @max: maximum value
   * Return: pointer to array of integers
   */
+int _abs(int n)
+{
+	n < 0 ? (n *= -1) : (n = n);
+	return (n);
+}
 int *array_range(int min, int max)
 {
 	int *p;
+	int absmin, absmax, i;
 
 	if (min > max)
 		return (NULL);
-	p = malloc((max - min) * sizeof(int));
+	absmin = _abs(min);
+	absmax = _abs(max);
+	if (absmin > absmax)
+		p = malloc((absmin - absmax) * sizeof(int));
+	else
+		p = malloc((absmax - absmin) * sizeof(int));
 	if (p == NULL)
 		return (NULL);
-	for ( ; min < max; min++)
-		p[min] = min;
+	for (i = 0; i < absmax; i++)
+		p[i] = min + i;
 	return (p);
 }
