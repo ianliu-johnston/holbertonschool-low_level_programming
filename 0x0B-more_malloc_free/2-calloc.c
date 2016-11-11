291d385565ec2298ec073cf156b218e750ddeee3
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-10 23:36:28 -0800

    Task 2: Fixed documentation

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 /**
   * _calloc - allocates memory for an initialized buffer
-  * @nmemb - number of memory bytes
-  * @size - number of data type bits
+  * @nmemb: number of memory bytes
+  * @size: number of data type bits
   * Return: return pointer to initialized buffer
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	p = malloc(size * nmemb);
 	if (p == NULL)
-		return(NULL);
+		return (NULL);
 	for (i = 0; i < nmemb ; i++)
 		p[nmemb] = 0;
 	return (p);
