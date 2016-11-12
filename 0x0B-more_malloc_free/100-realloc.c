15848103b9db0001d5328c1360d8237485405caf
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-11 23:06:32 -0800

    Task 100: Finished

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -1,10 +1,36 @@
 #include <stdlib.h>
 /**
-  * name - description
-  * @var - variable and what it does
-  * Return: return 0 on success
+  * _realloc - reallocates a memory block
+  * @ptr: pointer to the original memory block
+  * @old_size: old size of memory block
+  * @new_size: new size of memory block
+  * Return: pointer to reallocated memory block
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	return (0);
+	char *p, *castptr;
+	unsigned int i;
+
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	if (new_size > old_size)
+	{
+		p = malloc(new_size * sizeof(char));
+		castptr = ptr;
+		for (i = 0; i < old_size; i++)
+			p[i] = castptr[i];
+		free(ptr);
+		return (p);
+	}
+	return (ptr);
 }
