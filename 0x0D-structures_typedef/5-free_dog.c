d3fc5e22c5f3ca9f7636de3c146745f14805f6e5
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-14 18:18:04 -0800

    Task 5: fixed documentation

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -1,8 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
-  * free_dog - frees
-  * @void - describe argument
+  * free_dog - frees any memory allocated by dog.
+  * @d: pointer to struct
   * Return: what does it return?
   */
 void free_dog(dog_t *d)
