b5c85f8a5f4199f46f2531d49470df61b65a64ec
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-14 20:29:14 -0800

    Task 4: copied string literals into new memory

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -9,13 +9,28 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int nlen, olen, i;
 	dog_t *doggy;
 
+	for (nlen = 0; name[nlen]; nlen++)
+		;
+	for (olen = 0; owner[olen]; olen++)
+		;
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
-	doggy->name = name;
+
+	doggy->name = malloc((nlen + 1) * sizeof(doggy->name));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; doggy->name[i]; i++)
+		doggy->name[i] = name[i];
 	doggy->age = age;
-	doggy->owner = owner;
+
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; doggy->owner[i]; i++)
+		doggy->owner[i] = owner[i];
 	return (doggy);
 }
