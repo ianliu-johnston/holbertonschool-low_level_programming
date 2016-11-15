9bf9bfabdcb10f686cb94680aa44a68bc3417e89
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-14 17:26:59 -0800

    Task 2: shortened code with ternary operators

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -7,16 +7,10 @@
   */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d)
 	{
-		if (d->name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
+		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
 		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 	}
 }
