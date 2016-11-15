242dd918d5e4ab836f9d9ff8d3509bd8c07f05db
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-14 20:46:12 -0800

    Saving progress

---
+++ 0x0C-preprocessor/main.4.c
@@ -0,0 +1,11 @@
+#include "4-sum.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+}
--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -12,25 +12,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int nlen, olen, i;
 	dog_t *doggy;
 
-	for (nlen = 0; name[nlen]; nlen++)
+	nlen = olen = 0;
+	while (name[nlen++])
 		;
-	for (olen = 0; owner[olen]; olen++)
+	while (owner[olen++])
 		;
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
 
-	doggy->name = malloc((nlen + 1) * sizeof(doggy->name));
+	doggy->name = malloc(nlen * sizeof(doggy->name));
 	if (doggy == NULL)
 		return (NULL);
-	for (i = 0; doggy->name[i]; i++)
+	for (i = 0; i < nlen; i++)
 		doggy->name[i] = name[i];
+
 	doggy->age = age;
 
 	doggy->owner = malloc(olen * sizeof(doggy->owner));
 	if (doggy == NULL)
 		return (NULL);
-	for (i = 0; doggy->owner[i]; i++)
+	for (i = 0; i < olen; i++)
 		doggy->owner[i] = owner[i];
 	return (doggy);
 }
