6e86b815aed4a6407dd4e77bd08d7be23a9bf5b2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-16 15:50:45 -0800

    Task 1: trying something

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
   * array_iterator - executes a function on each element of an array
   * @array: input array
@@ -10,6 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int i = -1;
 
 	if (array)
+	{
 		while (++i < size)
+		{
+
 			action(array[i]);
+		}
+	}
 }
