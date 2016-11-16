4101fc5be482ec94c7aefb497e3469853fb6781d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-16 15:29:07 -0800

    Task 0: Fixed, update function_pointers.h

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,12 +1,9 @@
 #ifndef ASSIGN_0x0E
 #define ASSIGN_0x0E
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
 #endif
