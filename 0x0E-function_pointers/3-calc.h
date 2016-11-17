f54203cba477360207bea5193842603a84adb83c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-17 12:37:33 -0800

    Task 3: i don't know...

--- 0x0E-function_pointers/3-calc.h
+++ 0x0E-function_pointers/3-calc.h
@@ -8,20 +8,10 @@
   */
 typedef struct op
 {
-	char *op;
-	int (*f)(int, int);
-} opt_t;
-/**
-   * struct op - Struct op
-    *
-     * @op: The operator
-      * @f: The function associated
-       */
-typedef struct op
-{
-	    char *op;
-	        int (*f)(int a, int b);
+    char *op;
+    int (*f)(int a, int b);
 } op_t;
+
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
