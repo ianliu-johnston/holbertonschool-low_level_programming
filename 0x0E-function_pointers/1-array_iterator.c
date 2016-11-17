51eedf220f579f5453b2f51574f25d123d8ae128
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-16 16:09:11 -0800

    Task 1: changed back to undefined behavior

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stddef.h>
-#include <stdlib.h>
 /**
   * array_iterator - executes a function on each element of an array
   * @array: input array
@@ -9,11 +8,10 @@
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i = 0;
+	size_t i = -1;
 
 	if (array && action)
 	{
-		action(array[i]);
 		while (++i < size)
 			action(array[i]);
 	}
