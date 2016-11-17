6823b2f74bd4d21652602f1eff96fbc07704dc82
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-17 11:23:43 -0800

    Task 3: main - put function return into a variable

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -9,7 +9,7 @@
   */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int num1, num2, result;
 
 	if (argc != 4)
 	{
@@ -29,6 +29,7 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	printf("%d\n", get_op_func(argv[2])(num1, num2));
+	result = (get_op_func(argv[2])(num1, num2));
+	printf("%d\n", result);
 	return (0);
 }
