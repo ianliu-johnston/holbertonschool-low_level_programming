5fb3bb878cbc854906223957e9a0dfefd00f0c5e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-17 11:25:12 -0800

    Task 3: rearranged thing in main

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (*argv[3] == '0' && (*argv[2] == '%' || *argv[2] == '/'))
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	result = (get_op_func(argv[2])(num1, num2));
 	printf("%d\n", result);
 	return (0);
