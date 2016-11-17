605c5d628db0958da180335c89a252fd49f545d2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-16 16:38:24 -0800

    Task 2: Finished

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "dog.h"
+#include "function_pointers.h"
 /**
   * int_index - searches for an integer
   * @array: pointer to the array
@@ -10,6 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	i = 0;
-	return (i);
+	i = -1;
+	if (size <= 0)
+		return (-1);
+	while (++i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }
