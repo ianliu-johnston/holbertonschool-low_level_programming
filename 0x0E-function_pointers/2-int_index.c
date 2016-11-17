8ab051e01684ae5860d7e11742e1ce2f611bb79e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-16 16:42:02 -0800

    Task 2: Fixed

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -10,12 +10,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	i = -1;
+	i = 0;
 	if (size <= 0)
 		return (-1);
-	while (++i < size)
+	while (array[++i])
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i]) || cmp(array[0]))
 			return (i);
 	}
 	return (-1);
