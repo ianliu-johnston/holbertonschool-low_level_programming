c04242cc4c005c0c6ceb128f8edecd77c1411c94
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-17 12:57:38 -0800

    Task 3: Fixed spaces in header file.

--- 0x0E-function_pointers/3-calc.h
+++ 0x0E-function_pointers/3-calc.h
@@ -8,8 +8,8 @@
   */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int (*get_op_func(char *s))(int, int);
--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -9,26 +9,16 @@
   */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2;
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		printf("Error\n"), exit(98);
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	if (get_op_func(argv[2]) == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+		printf("Error\n"), exit(99);
 	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	result = (get_op_func(argv[2])(num1, num2));
-	printf("%d\n", result);
+		printf("Error\n"), exit(100);
+	printf("%d\n", (get_op_func(argv[2])(num1, num2)));
 	return (0);
 }
