fe4d52f8fd976ad036c9e57ba708e50515720991
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-18 13:26:09 -0800

    Task 2: Finished

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,9 +1,25 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 /**
-  * main - define function
-  * @void - describe argument
-  * Return: what does it return?
+  * print_strings - print a list of strings
+  * @separator: characters used to delimit
+  * @n: nmber of items to print
   */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	char *words;
+	va_list ap;
+
+	if (separator == NULL)
+		separator = "";
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		words = va_arg(ap, char *);
+		(words != NULL) ? printf("%s", words) : printf("(nil)");
+		(i + 1 != n) ? printf("%s", separator) : putchar('\n');
+	}
+	va_end(ap);
 }
