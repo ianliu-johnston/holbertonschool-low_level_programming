dddf1ea28a967c7cef52d57dd80f3b5b184d6932
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-18 12:34:01 -0800

    Task 0: finished

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 /**
   * sum_them_all - sums all arguments
   * @n: number of arguments
@@ -7,7 +9,14 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i;
+	unsigned int i, result;
+	va_list ap;
 
-	return (i);
+	va_start(ap, n);
+	for (i = result = 0; i < n; i++)
+	{
+		result += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (result);
 }
