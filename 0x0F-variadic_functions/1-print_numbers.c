b812354992e295303c9c97ab6aa332e698b5cff2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-18 15:47:44 -0800

    Task 1: Added another conditional to null separator checker.

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (separator != NULL)
+		if (separator != NULL && i + 1 != n)
 			printf("%s", separator);
 		if (i + 1 == n)
 			printf("\n");
