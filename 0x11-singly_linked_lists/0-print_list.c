0f9e423c6ec114572a6f0a2a110863ae2370f2b8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-28 01:37:02 -0800

    Initial commit

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+  * main - define function
+  * @void - describe argument
+  * Return: what does it return?
+  */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x11-singly_linked_lists/README.md
@@ -0,0 +1,9 @@
+# Holberton-School_0x11-singly_linked_lists
+
+0-print_list.c
+1-list_len.c
+2-add_node.c
+3-add_node_end.c
+4-free_list.c
+100-first.c
+lists.h
