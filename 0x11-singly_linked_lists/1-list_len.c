ad01f01f7e48ea05a7caadd058db11020d0f0951
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-28 13:02:46 -0800

    Task 1: Finished

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,10 +1,16 @@
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
 /**
-  * list_len - get length of the list
-  * @h: list
-  * Return: what does it return?
+  * print_list - finds number of elements in a linked list
+  * @h: singly linked list to print
+  * Return: number of elements in a linked list
   */
 size_t list_len(const list_t *h)
 {
-	return (0);
+	int i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
 }
