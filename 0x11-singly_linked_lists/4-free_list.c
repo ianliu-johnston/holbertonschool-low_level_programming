526c84de3ab662788c014f2d49ec94990ab471ad
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-28 18:11:56 -0800

    Task 4: Simplified code

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -1,19 +1,14 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 /**
   * free_list - frees a linked list
   * @head: the beginning of the list
   */
 void free_list(list_t *head)
 {
-	if (head->next == NULL)
-	{
-		free(head->str);
-		free(head);
+	if (head == NULL)
 		return;
-	}
 	free_list(head->next);
 	free(head->str);
 	free(head);
