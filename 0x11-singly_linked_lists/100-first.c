5b694545e2b60e320ff34020a3084e161b8754d0
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-28 18:52:04 -0800

    Task 100: Fixed

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
 void b(void) __attribute__ ((constructor));
 void b(void)
 {
-	printf("You're beat! and yet, you must allow,\n
+	printf("You're beat! and yet, you must allow,\n \
 			I bore my house upon my back!\n");
 }
