1ecf92d1468556fea6fa03453c2a6b951ccd51d9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-28 18:52:57 -0800

    Task 100: Fixed extra spaces in output

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -5,6 +5,5 @@
 void b(void) __attribute__ ((constructor));
 void b(void)
 {
-	printf("You're beat! and yet, you must allow,\n \
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
