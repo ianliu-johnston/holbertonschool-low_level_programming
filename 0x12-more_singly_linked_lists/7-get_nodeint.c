597a22e918ec3b207becc440de2fd171935974e1
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-01 11:21:56 -0800

    Task 7: added check to see if index > list

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
 	i = 0;
 	while (i < index)
 	{
+		if (head->next == NULL)
+			return (head);
 		head = head->next;
 		i++;
 	}
