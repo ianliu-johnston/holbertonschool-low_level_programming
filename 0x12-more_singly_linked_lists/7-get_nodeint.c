afe4152b2c65411362a920bbc3f89d6fe04f601f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-01 11:23:36 -0800

    Task 7: Fixed index > list with the correct return

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (i < index)
 	{
 		if (head->next == NULL)
-			return (head);
+			return (NULL);
 		head = head->next;
 		i++;
 	}
