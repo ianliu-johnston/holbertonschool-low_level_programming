399bb8ec446ed06359f27cd264f0dfceef9a51d5
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-30 16:57:58 -0800

    Header: Fixed incorrect function prototype

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -20,7 +20,7 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_list(listint_t *head);
+void free_listint(listint_t *head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
