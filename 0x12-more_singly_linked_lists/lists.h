ee947809d1483ef5079ed03df4082c90ff41122e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-01 17:34:24 -0800

    Updated betty style check

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -25,7 +25,7 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head,
-	   	unsigned int index, int n);
+	  unsigned int index, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
