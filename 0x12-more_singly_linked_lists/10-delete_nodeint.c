f77567a3bcadb9762281c71316e6e2d60904ad0d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-01 17:46:38 -0800

    Task 10: Finished documentation

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-  * insert_nodeint_at_index - insert a node at index
+  * delete_nodeint_at_index - delete a node at index
   * @head: head of the list
-  * @index: location to insert node
-  * @n: value of the inserted node
+  * @index: location of the node to delete
   * Return: pointer to head of list
   */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
