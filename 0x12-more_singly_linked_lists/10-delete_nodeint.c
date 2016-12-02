0753e07dc7fb9482c20f6dbc8f5e47fd7cd62807
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-01 17:47:39 -0800

    Task 10: Deleted stray printf

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,10 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current, *subsequent;
 
 	if (!head || !*head)
-	{
-		printf("Error\n");
 		return (-1);
-	}
 	current = *head;
 	if (index == 0)
 	{
