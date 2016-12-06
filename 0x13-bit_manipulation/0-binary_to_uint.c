bae88845f88d5135a85d3c6c1476282c666a261d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-05 19:59:47 -0800

    Task 0: Finished

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,26 @@
 #include "holberton.h"
-#include <stdio.h>
+/**
+  * _stoi - converts chars to ints
+  * @c: char to convert
+  * Return: converted int
+  */
+unsigned int _stoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+/**
+  * _strlen - calculates the length of the string
+  * @s: input
+  * Return: length of string
+  */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
 /**
   * binary_to_uint - converts a string of 1's and 0's to a decimal number
   * @b: string to convert
@@ -7,7 +28,20 @@
   */
 unsigned int binary_to_uint(const char *b)
 {
-	printf("%s\n", b);
-	return (0);
+	int i;
+	unsigned int result, tmp, expo;
+
+	if (!b)
+		return (0);
+	result = tmp = 0;
+	expo = 1;
+	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
+	}
+	return (result);
 }
 
