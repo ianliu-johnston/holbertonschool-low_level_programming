549cc1bd424f6d17aa24519af772c211a2adcc15
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-05 23:13:26 -0800

    Took out comment blocks

--- 0x13-bit_manipulation/mains/main.1.c
+++ 0x13-bit_manipulation/mains/main.1.c
@@ -8,19 +8,15 @@
  */
 int main(void)
 {
-	/*
     print_binary(0);
     printf("\n");
     print_binary(1);
     printf("\n");
-	*/
     print_binary(98);
     printf("\n");
-	/*
     print_binary(1024);
     printf("\n");
     print_binary((1 << 10) + 1);
     printf("\n");
-	*/
     return (0);
 }
