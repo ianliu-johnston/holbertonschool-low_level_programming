54c4f7b966e796b787cf1c3a4361959ab5fe0e1e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-06 13:44:42 -0800

    Task 3: Fixed betty style

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > (8 * sizeof(*n)) -1)
+	if (index > (8 * sizeof(*n)) - 1)
 		return (-1);
 	*n = *n | (1 << index);
 	return (1);
