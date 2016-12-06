6e540393c30a1eb3d65a92ef2f9c63a5cde75610
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-06 00:07:15 -0800

    Task 3: Fixed betty check

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
   * set_bit - sets bit at index to 1
   * @n: pointer to number
+  * @index: index to change
   * Return: 1 on success, -1 on failure
   */
 int set_bit(unsigned long int *n, unsigned int index)
