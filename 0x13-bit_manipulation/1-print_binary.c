06868e04fdb94e3819d9c22164178b488e1dc843
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-05 22:40:14 -0800

    Task 1: Testing

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -6,10 +6,12 @@
   */
 void print_binary(unsigned long int n)
 {
-	unsigned long int len, expo;
+	unsigned long int i, expo;
 
 	if (n == 0)
 		_putchar('0');
-	for (expo = 1, len = 0; expo <= n; len++, expo *= 2)
-		n & (1 << len) ? _putchar('1') : _putchar('0');
+	for (expo = 1, i = 0; expo <= n; expo *= 2, i++)
+		;
+	for (i--, expo = 1; i > 0; i--, expo *= 2)
+		(n & (1 << i)) ? _putchar('1') : _putchar('0');
 }
