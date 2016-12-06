7081011cc65a342e21051ec2b7ed415a8349930d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-06 00:51:58 -0800

    Task 5: save progress

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,13 +1,22 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-  * flip_bits - define function
+  * flip_bits - calculates the number of bits needed
+  * to get from one number to another
   * @void: describe argument
   * Return: 0 on success
   */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	n++;
-	m++;
-	return (0);
-}
+	unsigned long int expo;
+	unsigned int res, i;
 
+	if (n == m)
+		return (0);
+	for (expo = 1, i = res = 0; expo < (n ^ m); expo *= 2, i++)
+	{
+		if ((n ^ m) & (1 << i))
+			res++;
+	}
+	return (res);
+}
