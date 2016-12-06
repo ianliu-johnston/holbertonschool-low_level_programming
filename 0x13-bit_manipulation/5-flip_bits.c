1fbc018498334e1fe1f827e764bc0c29d973e529
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-06 00:58:25 -0800

    Task 5: Got expected output

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -3,20 +3,21 @@
 /**
   * flip_bits - calculates the number of bits needed
   * to get from one number to another
-  * @void: describe argument
-  * Return: 0 on success
+  * @n: base number
+  * @m: number to transform to
+  * Return: the number of bit transformations needed
   */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int expo;
 	unsigned int res, i;
 
-	if (n == m)
-		return (0);
-	for (expo = 1, i = res = 0; expo < (n ^ m); expo *= 2, i++)
+	res = 0;
+	for (expo = 1, i = 0; expo <= (n ^ m); expo *= 2, i++)
 	{
 		if ((n ^ m) & (1 << i))
 			res++;
+		printf("res = %u, i = %u, expo = %lu, expression: %lu\n", res, i, expo, (n ^ m) & (1 << i));
 	}
 	return (res);
 }
