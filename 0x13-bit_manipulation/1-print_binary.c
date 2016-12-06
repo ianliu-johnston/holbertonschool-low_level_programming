705368c55c2308d6a30f8031f5f41586de43214b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-05 22:05:16 -0800

    Task 1: Trying checker, but not finished

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
   * print_binary - converts a decimal number to binary
   * @n: integer to convert
   */
 void print_binary(unsigned long int n)
 {
-	n++;
-}
+	int i;
 
+	for (i = sizeof(unsigned long int) + 1; i >= 0; i--)
+		n & (1 << i) ? _putchar('1') : _putchar('0');
+}
