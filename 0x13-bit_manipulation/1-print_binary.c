5bb390c042d3b52c1648607a78805462ba972e58
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-06 12:22:44 -0800

    Task 1: Simplified logic with help from Gary and Julien

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,9 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
   * print_binary - converts a decimal number to binary
   * @n: integer to convert
   */
 void print_binary(unsigned long int n)
+{
+	int i, flag;
+
+	if (n == 0)
+		_putchar('0');
+	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			flag = 1;
+		if (flag == 1)
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
+	}
+}
+/*
 {
 	unsigned long int expo;
 	int i;
@@ -16,3 +31,4 @@ void print_binary(unsigned long int n)
 	for (; i >= 0; i--)
 		((n >> i) & 1) ? _putchar('1') : _putchar('0');
 }
+*/
