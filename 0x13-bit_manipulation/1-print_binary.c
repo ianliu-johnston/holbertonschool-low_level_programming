f020a1e7415c17224b644f3a9afaeac143cbf28f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-06 12:49:23 -0800

    Task 1: Removed block comment

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
   * print_binary - converts a decimal number to binary
   * @n: integer to convert
@@ -13,22 +12,8 @@ void print_binary(unsigned long int n)
 	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
 		if ((n >> i) & 1)
-			flag = 1;
+		flag = 1;
 		if (flag == 1)
 			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
 }
-/*
-{
-	unsigned long int expo;
-	int i;
-
-	if (n == 0)
-		_putchar('0');
-	for (expo = 1, i = 0; expo <= n; expo *= 2, i++)
-		;
-	i--;
-	for (; i >= 0; i--)
-		((n >> i) & 1) ? _putchar('1') : _putchar('0');
-}
-*/
