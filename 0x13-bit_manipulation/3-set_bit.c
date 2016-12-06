6a494d50f3d6d7c472481c9c18d0f0bc6f9ebeb9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-06 04:11:59 -0800

    Task 3: Reconfigured failure condition

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -8,5 +8,5 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	*n = *n ^ (1 << index);
-	return ((index > sizeof(unsigned int) || index == 0) ? 1 : -1);
+	return ((index > 8 * sizeof(*n)) ? -1 : 1);
 }
