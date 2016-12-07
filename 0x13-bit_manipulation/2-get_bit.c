293e9ad91d6e6814a8292a4879f473630998746d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-07 10:04:19 -0800

    Fixed search over 64 chars to 63 chars

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -7,5 +7,5 @@
   */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((index > 8 * sizeof(n)) ? -1 : (int)(n >> index) & 1);
+	return ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1);
 }
