02c4c048ec4bcc3885af1be14d1125caf1d2503d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-08 12:59:52 -0800

    Task 1: Added open check

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,6 +13,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	new_file = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if (new_file == 0)
+		return (-1);
 	for (len = 0; text_content[len]; len++)
 		;
 	write(new_file, text_content, len);
