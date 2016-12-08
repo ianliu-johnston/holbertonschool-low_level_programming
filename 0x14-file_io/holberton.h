8a8c554321c33e4ab920ca65867e16d56f2e07da
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-08 12:12:18 -0800

    Added more header declarations

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -2,6 +2,9 @@
 #define HEADER_H
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #define BUFSIZE 100
 ssize_t read_textfile(const char *filename, size_t letters);
