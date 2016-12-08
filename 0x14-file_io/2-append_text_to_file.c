a6a73e0a0f214ff2400ba43efbc9d5070b3cf09e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-08 12:56:20 -0800

    Task 2: Removed one line from the documentation. Maybe it fixed requirement checks?

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -4,7 +4,6 @@
   * @filename: file to append to, if NULL, return -1
   * @text_content: content to append, if NULL, do not append
   * Return: 1 on success, -1 on failure
-  * failure if filename is NULL, or if no permission
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
