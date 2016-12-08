4d780cd5fa27efe94d59ffd65160fa3dd7eb7f6f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-08 13:03:01 -0800

    Task 2: Changed open check to be -1

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	append_file = open(filename, O_WRONLY | O_APPEND);
-	if (append_file == 0)
+	if (append_file == -1)
 		return (-1);
 	for (len = 0; text_content[len]; len++)
 		;
