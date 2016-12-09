99d5101f47fdfabd60fa7a7627e97e5c8c18eb0c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-08 18:18:44 -0800

    Task 1: Added truncate to open()

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	new_file = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (new_file == -1)
 		return (-1);
 	if (text_content == NULL)
