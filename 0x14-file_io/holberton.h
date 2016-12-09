afff050b5e6ce07ef606501f15e75b9f10fb0753
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-08 17:37:17 -0800

    Fixed header file

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -6,7 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#define BUFFERSIZE 1204
+#define BUFSIZE 1204
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
