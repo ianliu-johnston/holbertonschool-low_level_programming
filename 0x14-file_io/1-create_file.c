bfebba897201284d43964395b66769a4cf7a1767
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-08 18:14:11 -0800

    Task 1: added a close

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,12 +10,15 @@ int create_file(const char *filename, char *text_content)
 	int new_file, len, wr_stat;
 
 	if (filename == NULL)
-	   	return (-1);
+		return (-1);
 	new_file = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (new_file == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
+		close(new_file);
 		return (1);
+	}
 	for (len = 0; text_content[len]; len++)
 		;
 	wr_stat = write(new_file, text_content, len);
