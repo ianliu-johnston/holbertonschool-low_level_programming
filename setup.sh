660ed24e2ba14e793f8781cc525aa32d26a763be
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-02 23:59:14 -0800

    Removed a bug from setup.sh

--- setup.sh
+++ setup.sh
@@ -54,9 +54,7 @@ B=$(grep -n "<h2>" $INPUT | grep -A1 "Readme" | cut -d : -f 1 | tail -1)
 tail -n +$A $INPUT | head -n $(($B-$A)) | grep "<a href=" | sed 's/<a href=\"/\n/g' | grep "http"| cut -d \" -f1 | sed 's/^/* [link](/;s/$/)/' >> README.md
 echo "" >> README.md
 echo "## Description of Files" >> README.md
-head -8 README.md > README.md.tmp
-ls -1 | grep "[0-9]-" | sort -h | sed 's/^/<h6>/g;s/$/<\/h6>\n/g' >> README.md.tmp
-mv README.md.tmp README.md
+ls -1 | grep "[0-9]-" | sort -h | sed 's/^/<h6>/g;s/$/<\/h6>\n/g' >> README.md
 #MAINS
 mkdir mains
 cd mains/
