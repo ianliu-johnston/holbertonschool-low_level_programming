f869fd731eebf86f8e75730d57b9f157c2661518
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-09 01:03:28 -0800

    Task 8: Updating main

--- 0x16-doubly_linked_lists/mains/main.8.c
+++ 0x16-doubly_linked_lists/mains/main.8.c
@@ -8,61 +8,23 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
-int main(void)
+int main(int ac, char *av[])
 {
     dlistint_t *head;
-
+	if (ac != 2)
+		printf("Error\n"), exit(1);
     head = NULL;
     add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
     add_dnodeint_end(&head, 2);
     add_dnodeint_end(&head, 3);
     add_dnodeint_end(&head, 4);
-    add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
+    add_dnodeint_end(&head, 5);
+    add_dnodeint_end(&head, 6);
+    add_dnodeint_end(&head, 7);
     print_dlistint(head);
     printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 5);
-    print_dlistint(head);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    print_dlistint(head);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    print_dlistint(head);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    print_dlistint(head);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    print_dlistint(head);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    print_dlistint(head);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    print_dlistint(head);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
+    delete_dnodeint_at_index(&head, atoi(av[1]));
     print_dlistint(head);
     return (0);
 }
