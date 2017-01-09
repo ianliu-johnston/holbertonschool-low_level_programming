0218ad3beacd803bd15be414fed0a6d212c59919
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-09 00:28:40 -0800

    Task 8: Finished

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,12 +1,32 @@
 #include "lists.h"
 /**
-  * delete_dnodeint_at_index - define function
-  * @void: describe argument
-  * Return: 0 on success
+  * delete_dnodeint_at_index - deletes a node at an index
+  * @head: double pointer to the head of the list
+  * @index: index of the node to delete
+  * Return: value of the node deleted
   */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	head++;
-	index++;
-	return (0);
+	dlistint_t *tmp_del, *tmp_head;
+
+	if (!head || !*head)
+		return (-1);
+	tmp_head = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		free(tmp_head);
+		return (1);
+	}
+	for ( ; index > 1 && tmp_head && tmp_head->next; index--)
+		tmp_head = tmp_head->next;
+	if (!tmp_head->next)
+		return (-1);
+	tmp_del = tmp_head->next;
+	tmp_head->next = tmp_del->next ? tmp_del->next : NULL;
+	if (tmp_del->next)
+		tmp_del->next->prev = tmp_head;
+	free(tmp_del);
+	return (1);
 }
