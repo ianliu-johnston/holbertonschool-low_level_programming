f68b0b7df6ed64bd3cdd7c6c8fd4244fa0d1ba75
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-03 23:10:06 -0800

    Task 1: It did not compile before. Fixed

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -10,7 +10,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long hash = 5381;
 	int c;
 
-	while (c = *str++)
+	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c;
 	return (hash);
 }
