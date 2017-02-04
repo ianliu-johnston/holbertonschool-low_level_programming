96b1b4a5ba18c84f0a53634b2ac49f296a9426de
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-04 11:24:53 -0800

    Task 3: <= 0

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 
 	new_node = NULL;
-	if (!ht || !key || strlen(key) <= 0 || *key < 0)
+	if (!ht || !key || strlen(key) <= 0 || *key <= 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = create_node(key, value);
