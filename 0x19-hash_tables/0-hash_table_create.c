b81ef094fad98fdd23c4f15ee6f9fe4eb8bc352c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-03 21:13:04 -0800

    Task 0: Complete, initialized to NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -8,9 +8,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	new_table = malloc((size + 1) * sizeof(hash_table_t));
+	if (size <= 0)
+		return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
-	new_table[size] = NULL;
+	new_table->size = size;
+	new_table->array = malloc((size) * sizeof(hash_node_t *));
+	if (!new_table->array)
+		return (NULL);
+	for (i = 0; i <= size; i++)
+		new_table->array[i] = NULL;
 	return (new_table);
 }
