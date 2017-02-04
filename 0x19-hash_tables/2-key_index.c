8a0c7fd8993a881fef2ae4b0bc2413f42c13bf9c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-03 23:37:21 -0800

    Task 2: Complete

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,10 +1,11 @@
-#include "hash_tables"
+#include "hash_tables.h"
 /**
-  * key_index - define function
-  * @void: describe argument
-  * Return: 0 on success
+  * key_index - gets the index of the key
+  * @key: the key to get the index from
+  * @size: the size of the hash table
+  * Return: the index of the string
   */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (0);
+	return (hash_djb2(key) % size);
 }
