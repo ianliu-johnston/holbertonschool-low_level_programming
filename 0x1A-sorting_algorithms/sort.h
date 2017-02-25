7083f0172ec0f288f8907ddadfe6f38c8bfec296
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-25 14:08:15 -0800

    Fixed header for betty

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -7,13 +7,14 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
+ * @prev: Pointer to the prev element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
