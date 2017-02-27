1cb25101f8a44540371b3fecdba8edf8570b83a1
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-26 22:36:47 -0800

    Task 1: Completegadd 1-insertion_sort_list.cgadd 1-insertion_sort_list.c!

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,41 +1,64 @@
 #include "sort.h"
+#include "1-insertion.h"
 
+listint_t *get_head(listint_t *list)
+{
+	listint_t *tmp;
+
+	tmp = list;
+	while (tmp->prev)
+		tmp = tmp->prev;
+	return(tmp);
+}
+
+int swap_dll(listint_t *left, listint_t *right)
+{
+	if (!left || !right)
+		return (EXIT_FAILURE);
+
+	if (left->prev)
+		left->prev->next = right;
+	if (right->next)
+		right->next->prev = left;
+	left->next = right->next;
+	right->prev = left->prev;
+	left->prev = right;
+	right->next = left;
+	return (EXIT_SUCCESS);
+}
 /**
   * insertion_sort_list - insertion sort algorithm
   * @list: Linked list to be sorted
   */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *big_step, *small_step, *right_slide;
+	unsigned int big_savespot, small_savespot, i;
+	listint_t *big_step, *small_step;
 
+	big_savespot = small_savespot = i = 0;
 	if (!*list || !list)
 		return;
 	big_step = small_step = *list;
-	/* Start at the second node */
-	big_step = big_step->next;
-	while (big_step->next)
+	while (big_step)
 	{
 		small_step = big_step;
-		while (small_step->prev && (small_step->prev)->n > small_step->n)
+		small_savespot = big_savespot;
+		while (small_step->prev && small_step->n < (small_step->prev)->n)
 		{
-			right_slide = small_step;
+			swap_dll(small_step->prev, small_step);
+
+			small_step = get_head(*list);
+			for (i = 0; i < small_savespot; i++)
+				small_step = small_step->next;
+
 			small_step = small_step->prev;
-
-			if (small_step->prev)
-				small_step->prev->next = right_slide;
-			if (right_slide->next)
-				right_slide->next->prev = small_step;
-
-			small_step->next = right_slide->next;
-			right_slide->prev = small_step->prev;
-			small_step->prev = right_slide;
-			right_slide->next = small_step;
-
-			if (right_slide->prev)
-				small_step = right_slide->prev;
-			print_list(*list);
+			small_savespot--;
+			print_list(get_head(*list));
 		}
+		big_step = get_head(*list);
+		for (i = 0; i < big_savespot; i++)
+			big_step = big_step->next;
 		big_step = big_step->next;
-		print_list(*list);
+		big_savespot++;
 	}
 }
