bbd881e79efa34a6e7ed6a02ff526ec2618c9ce1
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-19 15:00:06 -0700

    Fleshed out Create_Node

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,4 +1,4 @@
-#include "binary_trees"
+#include "binary_trees.h"
 /**
   * binary_tree_node - Creates a node in the tree.
   * @parent: the parent node
@@ -7,6 +7,13 @@
   */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	printf("Function Not Implemented. Your values: %p: %d\n", parent, value);
-	return (NULL);
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = new_node->right = NULL;
+	return (new_node);
 }
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -4,6 +4,7 @@
 /* Standard Library Calls */
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -86,7 +87,7 @@ avl_t *sorted_array_to_avl(int *array, size_t size);
 
 /* Binary Tree to heap */
 int binary_tree_is_heap(const binary_tree_t *tree);
-heap_t *heap_insert(heap_t **root, int value)
+heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
