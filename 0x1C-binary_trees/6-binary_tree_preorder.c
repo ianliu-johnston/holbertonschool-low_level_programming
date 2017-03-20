a933f3b961002369af216756660a5eb268c69064
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-20 16:24:54 -0700

    Task 6: Added the print function afterall

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -1,13 +1,14 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_preorder - define function
-  * @void: describe argument
-  * Return: 0 on success
+  * binary_tree_preorder - traverse through a tree
+  * @tree: Tree to traverse through
+  * @func: Function pointer to another
   */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
+	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
