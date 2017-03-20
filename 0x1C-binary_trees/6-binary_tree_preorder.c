57ff7793375fe4264c41baaa5219cc3caf918c18
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-20 16:16:15 -0700

    Task 6: Added print statement

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -8,6 +8,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree)
 		return;
+	printf("%d\n", tree->n);
 	binary_tree_preorder(tree->left, &(*func));
 	binary_tree_preorder(tree->right, &(*func));
 }
