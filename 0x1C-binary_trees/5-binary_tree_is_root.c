2f6b2536fde3da974e147d77147ffc8371ac9499
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-20 15:37:46 -0700

    Task 5: Fixed logic + documentation

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_is_root - define function
-  * @void: describe argument
-  * Return: 0 on success
+  * binary_tree_is_root - checks to see if root node
+  * @node: Node to check
+  * Return: 1 if root node, 0 all other cases
   */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	return (node->parent ? 0 : 1);
+	return (!node->parent ? 1 : 0);
 }
