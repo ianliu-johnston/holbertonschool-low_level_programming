a80f11ea5c846ab9bc6799f26c5aca5c82704718
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-20 16:29:30 -0700

    Task 7: Complete

--- 0x1C-binary_trees/7-binary_tree_inorder.c
+++ 0x1C-binary_trees/7-binary_tree_inorder.c
@@ -1,10 +1,15 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_inorder - define function
-  * @void: describe argument
+  * binary_tree_inorder - traverse the tree inorder
+  * @tree: Tree to traverse
+  * @func: Function pointer to printing function
   * Return: 0 on success
   */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	printf("Function Not Implemented. Your values: %p: %p\n", (void *)tree, func);
+	if (!tree || !func)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
