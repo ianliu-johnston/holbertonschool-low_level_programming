cb9c1c966f21b818f6597bf089e893ec672afd11
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-20 16:21:28 -0700

    Task 6: Added check for null

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -6,9 +6,8 @@
   */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree)
+	if (!tree || !func)
 		return;
-	printf("%d\n", tree->n);
-	binary_tree_preorder(tree->left, &(*func));
-	binary_tree_preorder(tree->right, &(*func));
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
