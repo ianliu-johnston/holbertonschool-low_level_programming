be4805465d5d902a1228a4e64be4ec0f51ff23d5
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-20 19:46:02 -0700

    Fixed style

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -1,15 +1,16 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_height - define function
-  * @void: describe argument
-  * Return: 0 on success
+  * binary_tree_height - returns the height of a binary tree
+  * @tree: Tree to measure
+  * Return: height of the tree
   */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t right, left;
+
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
 	left = binary_tree_height(tree->left) + 1;
 	right = binary_tree_height(tree->right) + 1;
-	return(right > left ? right : left);
+	return (right > left ? right : left);
 }
