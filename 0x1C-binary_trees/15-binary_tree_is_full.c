c379524fe6703e729f36eb111ba7f6ac32b3bd8c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-22 00:00:16 -0700

    Task 15: Complete. but why does it work?

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_is_root - define function
-  * @void: describe argument
+  * binary_tree_is_full - define function
+  * @tree: tree to measure
   * Return: 0 on success
   */
-int binary_tree_is_root(const binary_tree_t *node)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	printf("Function Not Implemented. %p\n", (void *)node);
-	return (0);
+	if (!tree)
+		return (0);
+
+	if (tree->left && tree->right)
+	{
+		if (binary_tree_is_full(tree->right) != 1)
+			return (0);
+		if (binary_tree_is_full(tree->left) != 1)
+			return (0);
+	}
+	return ((tree->right && !tree->left) || (!tree->right && tree->left) ? 0 : 1);
 }
