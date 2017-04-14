254a1575ac7fb3b433dbe0c7cecb11567477e4e9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-14 15:23:56 -0700

    Fixed style checks

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,19 +1,21 @@
 #include "search_algos.h"
 /**
-  * main - define function
-  * @void: describe argument
-  * Return: 0 on success
+  * linear_search - simplest search algorithm
+  * @array: input array
+  * @size: size of the array
+  * @value: value to search for
+  * Return: Index of value if found, -1 on errors
   */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if(!array)
+	if (!array)
 		return (-1);
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
-		if(array[i] == value)
+		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
